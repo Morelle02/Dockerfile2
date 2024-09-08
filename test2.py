3e7b88cefdd1d32fda5e602e0390b1047110c02c
Morelle Talognon <morelletalognon@gmail.com>
2024-09-08 13:22:12 +0100

    Modif2

--- test2.py
+++ test2.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 
-# Créer une instance de l'application Flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,7 +10,6 @@ def get_prenom():
     # Retourner le prénom sous forme de JSON
     return jsonify({'prenom': prenom})
 
-# Vérifier si le script est exécuté directement
 if __name__ == '__main__':
     # Lancer l'application Flask en mode debug
-    app.run(debug=True , host="0.0.0.0", port=5000)
+    app.run(debug=True)
